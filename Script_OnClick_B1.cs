c9b04bbe3729f20854b3b57b3c9c96c67f4c387f
EddieMay312 <131766992+EddieMay312@users.noreply.github.com>
2023-05-02 03:49:16 +0200

    Add files via upload

---
+++ Script_OnClick_B1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_OnClick_B1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Script_OnClick_B1 --> Start()");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Click()
+    {
+        print("B1 --> CLICK");
+    }
+}
---
+++ Script_OnClick_RR.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_OnClick_RR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Script_OnClick_RR --> Start()");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Click()
+    {
+        print("RR --> CLICK");
+    }
+}
+
